73dd84356828c8f349506ff82c9f351b70a9dcc7
Deniel <deniel.salvacion@dxc.com>
2022-09-16 13:24:51 +0800

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -21,7 +21,7 @@ const Input: React.FC<InputProps> = ({addToCollection}) => {
   <div className="container">
     <h2>Add TodoList</h2>
     <form id="addForm" className="form-inline mb-3" onSubmit={handleSubmit}>
-      <input onChange={handleChange} type="text" className="form-control mr-2" id="item" placeholder="Input Show/Movies"/>
+      <input onChange={handleChange} type="text" className="form-control mr-2" id="item" placeholder="Input list"/>
       <button onClick={() => addToCollection(input)} className="btn btn-dark">Submit</button>
     </form>
   </div>
